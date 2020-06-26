9d539ee92e17c885e8bc8582771132536c6c34c5
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-06-26 15:02:07 +0200

    Initial commit

---
+++ app-service-main.tf
@@ -0,0 +1,53 @@
+##############################
+## Azure App Service - Main ##
+##############################
+
+# Create a Resource Group
+resource "azurerm_resource_group" "appservice-rg" {
+  name     = "kopi-${var.region}-${var.environment}-${var.app_name}-app-service-rg"
+  location = var.location
+
+  tags = {
+    description = var.description
+    environment = var.environment
+    owner       = var.owner  
+  }
+}
+
+# Create the App Service Plan
+resource "azurerm_app_service_plan" "service-plan" {
+  name                = "kopi-${var.region}-${var.environment}-${var.app_name}-service-plan"
+  location            = azurerm_resource_group.appservice-rg.location
+  resource_group_name = azurerm_resource_group.appservice-rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+
+  tags = {
+    description = var.description
+    environment = var.environment
+    owner       = var.owner  
+  }
+}
+
+# Create the App Service
+resource "azurerm_app_service" "app-service" {
+  name                = "kopi-${var.region}-${var.environment}-${var.app_name}-app-service"
+  location            = azurerm_resource_group.appservice-rg.location
+  resource_group_name = azurerm_resource_group.appservice-rg.name
+  app_service_plan_id = azurerm_app_service_plan.service-plan.id
+
+  site_config {
+    linux_fx_version = "DOTNETCORE|3.1"
+  }
+
+  tags = {
+    description = var.description
+    environment = var.environment
+    owner       = var.owner  
+  }
+}
